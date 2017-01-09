4678f74d6559cfb25065154f32fcbe78962a19ce
jamesbenedict <jabenedi@indiana.edu>
2017-01-08 19:41:25 -0500

    pages finished, working on flipping

---
+++ dev/js/flipboard.js
@@ -0,0 +1,20 @@
+// on button click
+// loop through and find each .page in the html
+    // if class == page active
+        // reset class to page
+    // else 
+
+// $('#next').click(function(){
+//     $(this).css('color', 'red');
+// });
+
+$(document).ready(function() {	
+    // $('div.page').each(function(){
+    //     $(this).css('color', 'red');
+    // });
+    $('#next').click(function(){
+    	 $('#active').hide();
+	});
+
+   
+});
