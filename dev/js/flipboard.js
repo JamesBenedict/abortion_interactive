3b71962ce07787b112bc769ec388af5ff44b17ae
jamesbenedict <jabenedi@indiana.edu>
2017-01-12 16:10:11 -0500

    stopped next button from breaking the loop;

--- dev/js/flipboard.js
+++ dev/js/flipboard.js
@@ -1,14 +1,23 @@
 // on button click
 // loop through and find each .page in the html
 // on the next click, hide $this and display nexxt
-
-// $('#next').click(function(){
-//     $(this).css('color', 'red');
-// });
-
-// $('.body .page:nth-child(3)').attr('id', 'active');
 $( ".page" ).first().addClass('active');
+$( ".page" ).first().addClass('cover_page');
 $(".page").last().addClass('back_page');
+
+function checkNavigation() {
+	
+	if ($('.page').hasClass('cover_page')) {
+        $('.prev').hide();
+        $('#next').show();
+    } else if ($('.page').hasClass('back_page')) {
+        $('#next').hide();
+        $('.prev').show();
+    } else {
+        $('.prev').show();
+        $('#next').show();
+    }
+}
 // no slide up but looping
 // $('#next').click(function(){
 // 	 // $('#active').hide();
@@ -17,28 +26,37 @@ $(".page").last().addClass('back_page');
 // 	 $toShow.addClass('active');
 // });
 
-$('#next').click(function(){
 
-	 if($(this).hasClass('.back_page')){
-	 	$('#next').hide();
-
-	 } else{
+var flipbook = function () {
+	$('#next').click(function(){
+	checkNavigation();
 	 	 var $toShow = $('.active').next('.page').length > 0 ?  $('.active').next('.page') : $('#cover_page');
-		 $('.active').slideUp('slow', function() {
+		 $('.active').fadeOut('slow', function() {
 		 	$(this).remove();
 		 });
 		 // removeClass('active');
 		 $toShow.fadeIn('slow', function() {
 		 	$(this).addClass('active');
 		 });
+	});
+
+	// $('#prev').click(function(){
+	// 	checkNavigation();
+	//  	 var $toShow = $('.active').prev('.page').length > 0 ?  $('.active').prev('.page') : $('#cover_page');
+	// 	 $('.active').slideDown('slow', function() {
+	// 	 	$(this).remove();
+	// 	 });
+	// 	 // removeClass('active');
+	// 	 $toShow.fadeOut('slow', function() {
+	// 	 	$(this).addClass('active');
+	// 	 });
+	// });
+ 
+};
+
+
+$(document).ready(flipbook);
 
 
 
-	 }
-
-
-	
-
-	 
-});
 
