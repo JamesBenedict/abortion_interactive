1e2372df2f1b45de3f8d5fc5e0d917cc0e3d7609
jamesbenedict <jabenedi@indiana.edu>
2017-01-12 14:54:38 -0500

    forward button working

--- dev/js/flipboard.js
+++ dev/js/flipboard.js
@@ -6,13 +6,24 @@
 //     $(this).css('color', 'red');
 // });
 
-$(document).ready(function() {	
-    // $('div.page').each(function(){
-    //     $(this).css('color', 'red');
-    // });
-    $('#next').click(function(){
-    	 $('#active').hide();
-	});
+// $('.body .page:nth-child(3)').attr('id', 'active');
+$( ".page" ).first().addClass('active');
 
-   
+// $("a.next").click(function() {
+//     $('.first').toggleClass("first");
+//     var $toHighlight = $('.doShow').next('li').length > 0 ? $('.doShow').next('li') : $('.event_months li').first();
+//     $('.doShow').removeClass('doShow');
+//     $toHighlight.addClass('doShow');
+// });
+
+
+$('#next').click(function(){
+	 // $('#active').hide();
+	 var $toShow = $('.active').next('.page');
+	  // > 0 ? $('#first');
+	 $('.active').removeClass('active');
+	 $toShow.addClass('active');
 });
+
+
+
