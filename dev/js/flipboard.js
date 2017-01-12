a6edaf894faf1a6f056fc406e837b28da3d97156
jamesbenedict <jabenedi@indiana.edu>
2017-01-12 18:59:04 -0500

    relative positioning

--- dev/js/flipboard.js
+++ dev/js/flipboard.js
@@ -1,80 +1,22 @@
 // on button click
 // loop through and find each .page in the html
 // on the next click, hide $this and display nexxt
-
-// no slide up but looping
-// $('#next').click(function(){
-// 	 // $('#active').hide();
-// 	 var $toShow = $('.active').next('.page').length > 0 ?  $('.active').next('.page') : $('#cover_page');
-// 	 $('.active').removeClass('active');
-// 	 $toShow.addClass('active');
-// });
-
-
-// $( ".page" ).first().addClass('active');
-// $( ".page" ).first().addClass('cover_page');
-// $(".page").last().addClass('back_page');
-
-// function checkNavigation() {
-	
-// 	if ($('.page').hasClass('cover_page')) {
-//         $('.prev').hide();
-//         $('#next').show();
-//     } else if ($('.page').hasClass('back_page')) {
-//         $('#next').hide();
-//         $('.prev').show();
-//     } else {
-//         $('.prev').show();
-//         $('#next').show();
-//     }
-// }
-
-// var flipbook = function () {
-// 	$('#next').click(function(){
-// 	checkNavigation();
-// 	 	 var $toShow = $('.active').next('.page').length > 0 ?  $('.active').next('.page') : $('#cover_page');
-// 		 $('.active').fadeOut('slow', function() {
-// 		 	$(this).remove();
-// 		 });
-// 		 // removeClass('active');
-// 		 $toShow.fadeIn('slow', function() {
-// 		 	$(this).addClass('active');
-// 		 });
-// 	});
-
-	// $('#prev').click(function(){
-	// 	checkNavigation();
-	//  	 var $toShow = $('.active').prev('.page').length > 0 ?  $('.active').prev('.page') : $('#cover_page');
-	// 	 $('.active').slideDown('slow', function() {
-	// 	 	$(this).remove();
-	// 	 });
-	// 	 // removeClass('active');
-	// 	 $toShow.fadeOut('slow', function() {
-	// 	 	$(this).addClass('active');
-	// 	 });
-	// });
- 
-// };
-// $(document).ready(flipbook);
-
-
-$( ".page" ).first().addClass('active-slide');
+// http://jsfiddle.net/794f4yvw/12/
+$( ".page" ).first().addClass('active');
 $( ".page" ).first().addClass('first');
 $(".page").last().addClass('last');
 
 function checkNavigation() {
-
-    if ($('.active-slide').hasClass('first')) {
+    if ($('.active').hasClass('first')) {
         $('#prev').hide();
         $('#next').show();
-    } else if ($('.active-slide').hasClass('last')) {
+    } else if ($('.active').hasClass('last')) {
         $('#next').hide();
         $('#prev').show();
     } else {
         $('#prev').show();
         $('#next').show();
     }
-
 }
 
 var main = function () {
@@ -82,32 +24,40 @@ var main = function () {
     
     $('#next').click(function () {
         
-        var currentSlide = $('.active-slide');
-        var nextSlide = currentSlide.next('.page');
+        var activePage = $('.active');
+        var nextPage = activePage.next('.page');
 
-        //if nextslide is last slide, go back to the first
-        if (nextSlide.length === 0) {
-            nextSlide = $('.page').first();
+        
+        if (nextPage.length === 0) {
+            nextPage = $('.page').first();
         }
 
-        currentSlide.fadeOut(500).removeClass('active-slide');
-        nextSlide.fadeIn(1100).addClass('active-slide');
+        activePage.slideUp(500).removeClass('active');
+        nextPage.slideDown(1100).addClass('active');
+
+        // activePage.effect("slide", {"direction" : "up", "mode" : "hide"} , 600).removeClass('active');
+
+        // nextPage.effect("slide", {"direction" : "down", "mode" : "show"} , 1200).addClass('active');
 
         checkNavigation();
     });
 
     //prev slide function
     $('#prev').click(function () {
-        var currentSlide = $('.active-slide');
-        var prevSlide = currentSlide.prev('.page');
+        var activePage = $('.active');
+        var prevSlide = activePage.prev('.page');
 
         if (prevSlide.length === 0) {
             prevSlide = $('.page').last();
         }
 
-        currentSlide.fadeOut(600).removeClass('active-slide');
-        prevSlide.fadeIn(600).addClass('active-slide');
-         checkNavigation();
+        activePage.slideUp(1200).removeClass('active');
+        prevSlide.slideDown(1200).addClass('active');
+
+        // activePage.effect("slide", {"direction" : "up", "mode" : "hide"} , 600).removeClass('active');
+        // nextPage.effect("slide", {"direction" : "down", "mode" : "show"} , 1200).addClass('active');
+
+        checkNavigation();
     });
    
 
