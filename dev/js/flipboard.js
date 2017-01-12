21983a7f7d7fc3cce74c52b37a38aeda7a923024
jamesbenedict <jabenedi@indiana.edu>
2017-01-12 16:15:53 -0500

    redid flibook logicc, this contains comments of the old way too

--- dev/js/flipboard.js
+++ dev/js/flipboard.js
@@ -1,23 +1,7 @@
 // on button click
 // loop through and find each .page in the html
 // on the next click, hide $this and display nexxt
-$( ".page" ).first().addClass('active');
-$( ".page" ).first().addClass('cover_page');
-$(".page").last().addClass('back_page');
 
-function checkNavigation() {
-	
-	if ($('.page').hasClass('cover_page')) {
-        $('.prev').hide();
-        $('#next').show();
-    } else if ($('.page').hasClass('back_page')) {
-        $('#next').hide();
-        $('.prev').show();
-    } else {
-        $('.prev').show();
-        $('#next').show();
-    }
-}
 // no slide up but looping
 // $('#next').click(function(){
 // 	 // $('#active').hide();
@@ -27,18 +11,36 @@ function checkNavigation() {
 // });
 
 
-var flipbook = function () {
-	$('#next').click(function(){
-	checkNavigation();
-	 	 var $toShow = $('.active').next('.page').length > 0 ?  $('.active').next('.page') : $('#cover_page');
-		 $('.active').fadeOut('slow', function() {
-		 	$(this).remove();
-		 });
-		 // removeClass('active');
-		 $toShow.fadeIn('slow', function() {
-		 	$(this).addClass('active');
-		 });
-	});
+// $( ".page" ).first().addClass('active');
+// $( ".page" ).first().addClass('cover_page');
+// $(".page").last().addClass('back_page');
+
+// function checkNavigation() {
+	
+// 	if ($('.page').hasClass('cover_page')) {
+//         $('.prev').hide();
+//         $('#next').show();
+//     } else if ($('.page').hasClass('back_page')) {
+//         $('#next').hide();
+//         $('.prev').show();
+//     } else {
+//         $('.prev').show();
+//         $('#next').show();
+//     }
+// }
+
+// var flipbook = function () {
+// 	$('#next').click(function(){
+// 	checkNavigation();
+// 	 	 var $toShow = $('.active').next('.page').length > 0 ?  $('.active').next('.page') : $('#cover_page');
+// 		 $('.active').fadeOut('slow', function() {
+// 		 	$(this).remove();
+// 		 });
+// 		 // removeClass('active');
+// 		 $toShow.fadeIn('slow', function() {
+// 		 	$(this).addClass('active');
+// 		 });
+// 	});
 
 	// $('#prev').click(function(){
 	// 	checkNavigation();
@@ -52,11 +54,66 @@ var flipbook = function () {
 	// 	 });
 	// });
  
+// };
+// $(document).ready(flipbook);
+
+
+$( ".page" ).first().addClass('active-slide');
+$( ".page" ).first().addClass('first');
+$(".page").last().addClass('last');
+
+function checkNavigation() {
+
+    if ($('.active-slide').hasClass('first')) {
+        $('#prev').hide();
+        $('#next').show();
+    } else if ($('.active-slide').hasClass('last')) {
+        $('#next').hide();
+        $('#prev').show();
+    } else {
+        $('#prev').show();
+        $('#next').show();
+    }
+
+}
+
+var main = function () {
+    checkNavigation();
+    
+    $('#next').click(function () {
+        
+        var currentSlide = $('.active-slide');
+        var nextSlide = currentSlide.next('.page');
+
+        //if nextslide is last slide, go back to the first
+        if (nextSlide.length === 0) {
+            nextSlide = $('.page').first();
+        }
+
+        currentSlide.fadeOut(500).removeClass('active-slide');
+        nextSlide.fadeIn(1100).addClass('active-slide');
+
+        checkNavigation();
+    });
+
+    //prev slide function
+    $('#prev').click(function () {
+        var currentSlide = $('.active-slide');
+        var prevSlide = currentSlide.prev('.page');
+
+        if (prevSlide.length === 0) {
+            prevSlide = $('.page').last();
+        }
+
+        currentSlide.fadeOut(600).removeClass('active-slide');
+        prevSlide.fadeIn(600).addClass('active-slide');
+         checkNavigation();
+    });
+   
+
 };
 
-
-$(document).ready(flipbook);
-
+$(document).ready(main);
 
 
 
